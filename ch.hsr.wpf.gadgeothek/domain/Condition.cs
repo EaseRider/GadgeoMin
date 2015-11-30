1c8312e90699493e4b8fce9f9dc985bed7a960a4
Alexis.Suter <alexis.suter@hsr.ch>
2015-11-30 13:38:58 +0100

    fsadf

--- ch.hsr.wpf.gadgeothek/domain/Condition.cs
+++ ch.hsr.wpf.gadgeothek/domain/Condition.cs
@@ -1,7 +1,7 @@
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-
+// my ass Comment
 namespace ch.hsr.wpf.gadgeothek.domain
 {
     [JsonConverter(typeof(StringEnumConverter))]
